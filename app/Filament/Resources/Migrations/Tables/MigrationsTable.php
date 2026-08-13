2439c19ebade2965fab4814cbb375ae3f86376df
Putri Suardina <putrisuardina02@gmail.com>
2026-08-13 11:47:47 +0700

    first commit

---
+++ app/Filament/Resources/Migrations/Tables/MigrationsTable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Migrations\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class MigrationsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('migration')
+                    ->label('Migration')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('batch')
+                    ->label('Batch')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
